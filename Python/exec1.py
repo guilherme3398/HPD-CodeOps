d76891024ea15c33ed43d7d55cd2b968b56974b1
Guilherme Othon <guilherme3398@gmail.com>
2018-05-02 21:23:12 -0300

    Class Python Part1,2,3

---
+++ Python/exec1.py
@@ -0,0 +1,3 @@
+
+print('Qual o seu nome?')nome = input()print('Qual a sua idade?')idade = input()print('Seja muito bem-vindo ao treinamento, %s' % nome) # INTERPOLANDOprint('Ja esta ficando velhor hein, ' + str(idade) + ' anos ja esta no final da vida!') # CONCATENANDOprint('Qual o seu sexo, %s?' % nome)sexo = input()print('Fiquei surpreso %s, nao me parece do sexo %s' % (nome, sexo)) # PYTHON 3.5#print(f"Fiquei surpreso {nome}, nao me parece do sexo {sexo}") # PYTHON 3.6 pra cimaprint("apenas testando as variaveis do nome {} e do sexo {}".format(nome, sexo))print("apenas testando as variaveis do nome {nome} e do sexo {sexo}".format(nome="Godofredo", sexo="Alienigena"))valor = 10print("O valor do pao eh: %.2f " % valor)
+
